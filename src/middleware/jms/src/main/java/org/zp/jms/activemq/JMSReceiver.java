8914311e0fe4aa9fd3e777038a34b2369f088c0d
victor zhang <atalantis1024@163.com>
2016-11-29 21:02:51 +0800

    添加修改 ActiveMQ 范例

--- src/middleware/jms/src/main/java/org/zp/jms/activemq/JMSReceiver.java
+++ src/middleware/jms/src/main/java/org/zp/jms/activemq/JMSReceiver.java
@@ -1,28 +1,26 @@
-package org.zp.jms.activemq; /**
+/**
  * The Apache License 2.0
  * Copyright (c) 2016 Victor Zhang
  */
+package org.zp.jms.activemq;
 
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
-import javax.jms.DeliveryMode;
 import javax.jms.Destination;
-import javax.jms.MessageProducer;
+import javax.jms.MessageConsumer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
 /**
- * 消息的生产者
+ * 消息的消费者
  *
  * @author Victor Zhang
  * @date 2016/11/28.
  */
-public class Sender {
-    private static final int SEND_NUMBER = 4;
-
+public class JMSReceiver {
     public static void main(String[] args) {
         // ConnectionFactory ：连接工厂，JMS 用它创建连接
         ConnectionFactory connectionFactory;
@@ -30,12 +28,10 @@ public class Sender {
         Connection connection = null;
         // Session： 一个发送或接收消息的线程
         Session session;
-        // Destination ：消息的目的地
+        // Destination ：消息的目的地;消息发送给谁.
         Destination destination;
-        // MessageProducer：消息发送者
-        MessageProducer producer;
-        // TextMessage message;
-        // 构造ConnectionFactory实例对象，此处采用ActiveMq的实现jar
+        // 消费者，消息接收者
+        MessageConsumer consumer;
         connectionFactory = new ActiveMQConnectionFactory(
                 ActiveMQConnection.DEFAULT_USER,
                 ActiveMQConnection.DEFAULT_PASSWORD,
@@ -46,16 +42,19 @@ public class Sender {
             // 启动
             connection.start();
             // 获取操作连接
-            session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+            session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
             // 获取session注意参数值xingbo.xu-queue是一个服务器的queue，须在在ActiveMq的console配置
             destination = session.createQueue("FirstQueue");
-            // 得到消息生成者【发送者】
-            producer = session.createProducer(destination);
-            // 设置不持久化，此处学习，实际根据项目决定
-            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-            // 构造消息，此处写死，项目就是参数，或者方法获取
-            sendMessage(session, producer);
-            session.commit();
+            consumer = session.createConsumer(destination);
+            while (true) {
+                //设置接收者接收消息的时间，为了便于测试，这里谁定为100s
+                TextMessage message = (TextMessage) consumer.receive(100000);
+                if (null != message) {
+                    System.out.println("收到消息" + message.getText());
+                } else {
+                    break;
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -65,23 +64,5 @@ public class Sender {
             } catch (Throwable ignore) {
             }
         }
-    }
-
-    /**
-     * 发送消息
-     *
-     * @param session
-     * @param messageProducer 消息生产者
-     * @throws Exception
-     */
-    public static void sendMessage(Session session, MessageProducer messageProducer) throws Exception {
-        for (int i = 0; i < SEND_NUMBER; i++) {
-            //创建一条文本消息
-            TextMessage message = session.createTextMessage("ActiveMQ 发送消息" + i);
-            System.out.println("发送消息：Activemq 发送消息" + i);
-            //通过消息生产者发出消息
-            messageProducer.send(message);
-        }
-
     }
 }
--- src/middleware/jms/src/main/java/org/zp/jms/activemq/JMSSender.java
+++ src/middleware/jms/src/main/java/org/zp/jms/activemq/JMSSender.java
@@ -1,26 +1,28 @@
-/**
+package org.zp.jms.activemq; /**
  * The Apache License 2.0
  * Copyright (c) 2016 Victor Zhang
  */
-package org.zp.jms.activemq;
 
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
 import javax.jms.Destination;
-import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
 /**
- * 消息的消费者
+ * 消息的生产者
  *
  * @author Victor Zhang
  * @date 2016/11/28.
  */
-public class Receiver {
+public class JMSSender {
+    private static final int SEND_NUMBER = 4;
+
     public static void main(String[] args) {
         // ConnectionFactory ：连接工厂，JMS 用它创建连接
         ConnectionFactory connectionFactory;
@@ -28,10 +30,12 @@ public class Receiver {
         Connection connection = null;
         // Session： 一个发送或接收消息的线程
         Session session;
-        // Destination ：消息的目的地;消息发送给谁.
+        // Destination ：消息的目的地
         Destination destination;
-        // 消费者，消息接收者
-        MessageConsumer consumer;
+        // MessageProducer：消息发送者
+        MessageProducer producer;
+        // TextMessage message;
+        // 构造ConnectionFactory实例对象，此处采用ActiveMq的实现jar
         connectionFactory = new ActiveMQConnectionFactory(
                 ActiveMQConnection.DEFAULT_USER,
                 ActiveMQConnection.DEFAULT_PASSWORD,
@@ -42,19 +46,16 @@ public class Receiver {
             // 启动
             connection.start();
             // 获取操作连接
-            session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+            session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
             // 获取session注意参数值xingbo.xu-queue是一个服务器的queue，须在在ActiveMq的console配置
             destination = session.createQueue("FirstQueue");
-            consumer = session.createConsumer(destination);
-            while (true) {
-                //设置接收者接收消息的时间，为了便于测试，这里谁定为100s
-                TextMessage message = (TextMessage) consumer.receive(100000);
-                if (null != message) {
-                    System.out.println("收到消息" + message.getText());
-                } else {
-                    break;
-                }
-            }
+            // 得到消息生成者【发送者】
+            producer = session.createProducer(destination);
+            // 设置不持久化，此处学习，实际根据项目决定
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+            // 构造消息，此处写死，项目就是参数，或者方法获取
+            sendMessage(session, producer);
+            session.commit();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -64,5 +65,23 @@ public class Receiver {
             } catch (Throwable ignore) {
             }
         }
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param session
+     * @param messageProducer 消息生产者
+     * @throws Exception
+     */
+    public static void sendMessage(Session session, MessageProducer messageProducer) throws Exception {
+        for (int i = 0; i < SEND_NUMBER; i++) {
+            //创建一条文本消息
+            TextMessage message = session.createTextMessage("ActiveMQ 发送消息" + i);
+            System.out.println("发送消息：Activemq 发送消息" + i);
+            //通过消息生产者发出消息
+            messageProducer.send(message);
+        }
+
     }
 }
